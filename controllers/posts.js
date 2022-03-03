d3051b5842941b21cfd5a23fca54de0f71c2fc6a
MuGen9 <maksparadowski@gmail.com>
2022-03-03 16:41:26 +0100

    added env variables

--- controllers/posts.js
+++ controllers/posts.js
@@ -54,7 +54,7 @@ export const likePost = async (req, res) => {
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount++ }, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
     
     res.json(updatedPost);
 }
